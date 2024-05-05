1e33e0e21fa76787300b85c484726294ebed408f
RonexLemon <ronexondimu@gmail.com>
2024-05-05 16:26:48 +0300

    feat:login

--- wallet/src/app/page.tsx
+++ wallet/src/app/page.tsx
@@ -1,10 +1,12 @@
 import Image from "next/image";
 import LandingPage from "@/components/LandingPage";
+import Login from "@/components/Login";
 
 export default function Home() {
   return (
     <main className="h-screen  pt-10 bg-liskPrimary ">
-      <LandingPage/>
+      {/* <LandingPage/> */}
+      <Login/>
 
      
     </main>
---
+++ wallet/src/components/Main.tsx
@@ -0,0 +1,31 @@
+import React from "react";
+import Image from "next/image";
+import { Button } from "./ui/button";
+import Lisklogo from "../../public/Lsiklogo.svg"
+
+import { Input } from "@/components/ui/input"
+
+
+const Main =()=>{
+    return(
+        <div className=" w-full h-full flex flex-col justify-around  items-center">
+<Image src={Lisklogo} alt=""/>
+<h2>MAKE IT SAFE WITH LISK</h2>
+<div className="w-full flex flex-col items-center gap-8 justify-center">
+<Input type="password" placeholder="Passowrd" className="text-start w-3/4  border-b-2 border-collapse bg-transparent "/>
+<Button variant='default' size='lg' >UNSEAL</Button>
+
+</div>
+
+<div className="flex justify-end pr-10 items-end w-full">
+
+<Button variant='link'>Forget Password</Button>
+
+</div>
+            
+
+        </div>
+    )
+}
+
+export default Main;
